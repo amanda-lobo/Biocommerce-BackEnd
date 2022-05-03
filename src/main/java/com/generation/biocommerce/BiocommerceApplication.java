a5d91a83dcd3143fe33f7b3a00fa6f52c38158f6
brunacr <bruna.leitao.pro@gmail.com>
2022-05-03 09:59:16 -0300

    Criar o Projeto Spring

---
+++ src/main/java/com/generation/biocommerce/BiocommerceApplication.java
@@ -0,0 +1,13 @@
+package com.generation.biocommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiocommerceApplication.class, args);
+	}
+
+}
