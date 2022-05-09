c86b1ccef607576049254ae3e7072318d89ac15f
dogzeira <douglasdanilobutu13@gmail.com>
2022-05-09 13:45:46 -0300

    Criou A Classe Model Usuario

--- src/main/java/com/generation/biocommerce/model/Usuario.java
+++ src/main/java/com/generation/biocommerce/model/Usuario.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_usuario")
-public class usuario {
+public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
