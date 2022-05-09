e0e23f7901d904e9c6a668b6f80806e20ca3b8e1
Creiton <thalmaicorreia7@gmai.com>
2022-05-09 12:08:32 -0300

    Atualizou classes

--- src/main/java/com/generation/biocommerce/model/Categoria.java
+++ src/main/java/com/generation/biocommerce/model/Categoria.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.Size;
 
 @Entity //como se fosse o create table 
 @Table(name = "tb_categoria")//dá nome para a tabela 
-public class BioCommerce {
+public class Categoria {
 	@Id //chave primaria
 	@GeneratedValue(strategy = GenerationType.IDENTITY)//é o auto incremento 
 	private Long id;
